afcc253b77984f96713090cfab0d2803d62f9662
Kesavan <kesavan6002@gmail.com>
2021-11-26 14:07:57 -0800

    xyhvector class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/XyhVector.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+public class XyhVector {
+    public double x;
+    public double y;
+    public double h;
+    XyhVector(double x, double y, double h){
+        this.y = y;
+        this.x = x;
+        this.h = h;
+    }
+}

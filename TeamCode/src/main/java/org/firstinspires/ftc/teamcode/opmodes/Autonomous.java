42fefb5d9aa46e8179e517e4cad6acba09d14da4
Showmen Talukder <38407270+SoulSen@users.noreply.github.com>
2022-01-24 21:07:09 -0800

    auton fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Autonomous.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.opmodes;
+package org.firstinspires.ftc.teamcode.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
@@ -15,39 +15,33 @@ import java.util.List;
 
 @com.qualcomm.robotcore.eventloop.opmode.Autonomous()
 public class Autonomous extends LinearOpMode {
-    private Robot robot;
+    private static final int THRESHOLD = 3;
 
+    private Robot robot;
     public RobotHardware hardware;
+
+    private GoToPosition goToCarousel;
+    private GoToPosition rotateCarousel;
+    private GoToPosition goToShippingHub;
+
+    private SequentialMovements goToWarehouse;
 
     List<Task> tasks = new ArrayList<>();
 
     @Override
     public void runOpMode() {
         tasks.add(arg -> spinCarousel((Long) arg));
-        //tasks.add(arg -> goDropShippingHub());
-        //tasks.add(arg -> goToWarehouse());
+        tasks.add(arg -> goDropShippingHub());
+        tasks.add(arg -> goToWarehouse());
 
         robot = new Robot(this);
         hardware = robot.hardware;
 
         robot.movement.toggleClaw(0.675);
 
-        waitForStart();
-
-        long timestamp = System.currentTimeMillis() / 1000;
-
-        while(opModeIsActive() && !isStopRequested()) {
-            hardware.odometry();
-            doNextTask(timestamp);
-            telemetry.addData("X", hardware.pos.x);
-            telemetry.addData("Y", hardware.pos.y);
-            telemetry.addData("H", hardware.pos.h);
-            telemetry.update();
-        }
-    }
-
-    public boolean goToWarehouse() {
-        int threshold = 3;
+        this.goToCarousel = new GoToPosition(robot, new XyhVector(14,63,0), this);
+        this.rotateCarousel = new GoToPosition(robot, new XyhVector(14,63, Math.toRadians(-32)), this);
+        this.goToShippingHub = new GoToPosition(robot, new XyhVector(82,17,0), this);
 
         XyhVector crossUp = new XyhVector(13,-54, Math.toRadians(0));
         GoToPosition goToCrossUp = new GoToPosition(robot, crossUp, this);
@@ -63,23 +57,29 @@ public class Autonomous extends LinearOpMode {
         waypoints.put(goToWarehouse, false);
         waypoints.put(goToRotate, false);
 
-        SequentialMovements seqMovements = new SequentialMovements(waypoints, threshold, this);
+        this.goToWarehouse = new SequentialMovements(waypoints, THRESHOLD, this);
 
-        return seqMovements.runMovements();
+        waitForStart();
+
+        long timestamp = System.currentTimeMillis() / 1000;
+
+        while(opModeIsActive() && !isStopRequested()) {
+            doNextTask(timestamp);
+        }
+    }
+
+    public boolean goToWarehouse() {
+        return this.goToWarehouse.runMovements();
     }
 
     public boolean goDropShippingHub() {
-        int threshold = 3;
-        XyhVector shippingHubPos = new XyhVector(82,17,0);
-        GoToPosition goToShippingHub = new GoToPosition(robot, shippingHubPos, this);
-
         /*
         LiftMacro liftMacroUp = new LiftMacro(robot.movement, Lift.UP);
         Thread t1 = new Thread(liftMacroUp);
         t1.start();
          */
 
-        boolean finished = goToShippingHub.runWithPID(threshold);
+        boolean finished = this.goToShippingHub.runWithPID(THRESHOLD);
 
         if(finished /* && !t1.isAlive() */) {
             /*
@@ -96,21 +96,12 @@ public class Autonomous extends LinearOpMode {
     }
 
     public boolean spinCarousel(long start) {
-        int threshold = 3;
-        XyhVector carousel = new XyhVector(14,63,0);
-        GoToPosition goToCarousel = new GoToPosition(robot, carousel, this);
-
-        boolean finished = goToCarousel.runWithPID(threshold);
+        boolean finished = this.goToCarousel.runWithPID(THRESHOLD);
 
         if(finished) {
-            telemetry.addLine("Gone to Carousel");
-            XyhVector rotatePos = new XyhVector(14,63, Math.toRadians(-32));
-            GoToPosition gotoRotate = new GoToPosition(robot, rotatePos, this);
-
-            boolean finishedRotate = gotoRotate.runWithPID(threshold);
+            boolean finishedRotate = this.rotateCarousel.runWithPID(THRESHOLD);
 
             if(finishedRotate) {
-                telemetry.addLine("Spinning Carousel");
                 if(hardware.flyWheelSpeed != 1) {
                     robot.movement.activateFlywheel(1);
                     return false;
